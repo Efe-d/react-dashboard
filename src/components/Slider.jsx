c386913629943ad45f07d4d0df618b6fdc9b8ced
unknown <edolavci@gmail.com>
2021-12-22 08:52:55 +0300

    slider bug fixed

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,145 +13,142 @@ import {
 /* 
 const [load,setLoad] = useState(false); */
 
-  
-
 function Slider() {
-     let movePer = 0;
-    const slides= document.getElementsByClassName("slide");
+  let movePer = 0;
+  const slides = document.getElementsByClassName("slide");
 
-    const slideWidth = document.querySelector(".slide");
-    const wrapperWidth = document.querySelector(".slider-in");
-    const totalCarousel = slides.length;
-    let maxPer = 0;
-  
-    const moveToNextCarousel = () => {
-      /* İleri butonuna her basışta kaç px gidecegini belirliyoruz */
-      /* Her Basışta slide cardinin widthi kadar transform oluyor */
-      movePer = movePer + slideWidth.clientWidth;
-  
-      /* Sona gelince başa atmasını saglıyoruz */
-      /* kaç tane slide oldugunu hesaplıyoruz. Sonra ekran boyutuna göre kaç tane card oldugunu hesaplatıp totalCarouselden eksil
+  const slideWidth = document.querySelector(".slide");
+  const wrapperWidth = document.querySelector(".slider-in");
+  const totalCarousel = slides.length;
+  let maxPer = 0;
+
+  const moveToNextCarousel = () => {
+    /* İleri butonuna her basışta kaç px gidecegini belirliyoruz */
+    /* Her Basışta slide cardinin widthi kadar transform oluyor */
+    movePer = movePer + slideWidth.clientWidth;
+
+    /* Sona gelince başa atmasını saglıyoruz */
+    /* kaç tane slide oldugunu hesaplıyoruz. Sonra ekran boyutuna göre kaç tane card oldugunu hesaplatıp totalCarouselden eksil
       -tiyoruz bunu yapmazsak ektradan boş ileri atacak */
-      let maxPer =
-        (totalCarousel - wrapperWidth.clientWidth / slideWidth.clientWidth) *
-        slideWidth.clientWidth;
-      for (let slide of slides) {
-        if (movePer > maxPer) {
-          movePer = 0;
-          slide.style.transform = "translateX(-" + movePer + "px)";
-        } else {
-          /* slide classlarına transform ekliyoruz */
-          slide.style.transform = "translateX(-" + movePer + "px)";
-        }
+    let maxPer =
+      (totalCarousel - wrapperWidth.clientWidth / slideWidth.clientWidth) *
+      slideWidth.clientWidth;
+    for (let slide of slides) {
+      if (movePer > maxPer) {
+        movePer = 0;
+        slide.style.transform = "translateX(-" + movePer + "px)";
+      } else {
+        /* slide classlarına transform ekliyoruz */
+        slide.style.transform = "translateX(-" + movePer + "px)";
       }
-    };
-  
-    const moveToPrevCarousel = () => {
-      movePer = movePer - slideWidth.clientWidth;
-      for (let slide of slides) {
-        if (movePer < 0) {
-          movePer = 0;
-        } else {
-          slide.style.transform = "translateX(-" + movePer + "px)";
-        }
+    }
+  };
+
+  const moveToPrevCarousel = () => {
+    movePer = movePer - slideWidth.clientWidth;
+    for (let slide of slides) {
+      if (movePer < 0) {
+        movePer = 0;
+      } else {
+        slide.style.transform = "translateX(-" + movePer + "px)";
       }
-    };
-  
+    }
+  };
 
   return (
-    <div>
-      <div className="slider-box">
-        <div className="slider">
-          <div className="header-slider">
-            <h3>Make Payments</h3>
-            <FontAwesomeIcon icon={faInfoCircle} size="1x" color="#6f3ecc" />
+    <div className="slider-box">
+      <div className="slider">
+        <div className="header-slider">
+          <h3>Make Payments</h3>
+          <FontAwesomeIcon icon={faInfoCircle} size="1x" color="#6f3ecc" />
+        </div>
+
+        <FontAwesomeIcon
+          icon={faAngleLeft}
+          size="4x"
+          color="#6f3ecc"
+          className="slider-icon"
+          onClick={moveToPrevCarousel}
+        />
+
+        <div className="slider-icon-mobile">
+          <div>
+            <FontAwesomeIcon
+              icon={faAngleLeft}
+              size="2x"
+              color="#6f3ecc"
+              className=""
+              onClick={moveToPrevCarousel}
+            />
           </div>
 
-          <FontAwesomeIcon
-            icon={faAngleLeft}
-            size="4x"
-            color="#6f3ecc"
-            className="slider-icon"
-            onClick={moveToPrevCarousel}
-          />
-
-          <div className="slider-icon-mobile">
-            <div>
-              <FontAwesomeIcon
-                icon={faAngleLeft}
-                size="2x"
-                color="#6f3ecc"
-                className=""
-                onClick={moveToPrevCarousel}
-              />
-            </div>
-
-            <div>
+          <div>
+            <button onClick={moveToNextCarousel}>
               <FontAwesomeIcon
                 icon={faAngleRight}
                 size="2x"
                 color="#6f3ecc"
                 className=""
-                onClick={moveToNextCarousel}
               />
+            </button>
+          </div>
+        </div>
+
+        <div className="slider-in">
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faDonate} size="3x" />
+              <h3>Donations & Charities</h3>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faHandHoldingMedical} size="3x" />
+              <h3>Insurence</h3>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faCreditCard} size="3x" />
+              <h3>Payment Services</h3>
             </div>
           </div>
 
-          <div className="slider-in">
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faDonate} size="3x" />
-                <h3>Donations & Charities</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faHandHoldingMedical} size="3x" />
-                <h3>Insurence</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faCreditCard} size="3x" />
-                <h3>Payment Services</h3>
-              </div>
-            </div>
-
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
-                <h3>Visa & Embassy Payments</h3>
-              </div>
-            </div>
-
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
-                <h3>Visa & Embassy Payments</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
-                <h3>Visa & Embassy Payments</h3>
-              </div>
-            </div>
-            <div className="slide">
-              <div className="slider-icons">
-                <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
-                <h3>Visa & Embassy Payments</h3>
-              </div>
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
+              <h3>Visa & Embassy Payments</h3>
             </div>
           </div>
 
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
+              <h3>Visa & Embassy Payments</h3>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
+              <h3>Visa & Embassy Payments</h3>
+            </div>
+          </div>
+          <div className="slide">
+            <div className="slider-icons">
+              <FontAwesomeIcon icon={faMoneyCheckAlt} size="3x" />
+              <h3>Visa & Embassy Payments</h3>
+            </div>
+          </div>
+        </div>
+
+        <button onClick={moveToNextCarousel}>
           <FontAwesomeIcon
             icon={faAngleRight}
             size="4x"
             color="#6f3ecc"
             className="slider-icon"
-            onClick={moveToNextCarousel}
           />
-        </div>
+        </button>
       </div>
     </div>
   );
